bc0480f7d765146173f69af489c7f8b528081556
goma2 <124752584+goma2@users.noreply.github.com>
2023-03-31 18:11:27 +0800

    Add files via upload

---
+++ mini calculator-2.py
@@ -0,0 +1,12 @@
+num_1 = float(input("Enter a number: "))
+num_2 = float(input("Enter a number: "))
+
+add = num_1 + num_2
+subtract = num_1 - num_2
+multiply = num_1 * num_2
+divide = num_1 / num_2
+
+print("Add", add)
+print("Subtract", subtract)
+print("Multiply", multiply)
+print("Divide", divide)
